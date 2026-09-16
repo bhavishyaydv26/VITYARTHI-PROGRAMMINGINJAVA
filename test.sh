c58beae836e20489b8933ead96156f2514fd4648
Bhavishya <bhavishyaahir345@gmail.com>
2026-09-16 19:02:34 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Compile and run the test suite (macOS / Linux)
-set -e
-mkdir -p out
-javac -d out $(find src -name "*.java")
-java -cp out com.vityarthi.expensetracker.test.TestRunner
